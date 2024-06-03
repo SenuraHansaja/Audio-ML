153f88254248e691bd99e4cd480445ffcae15106
Senura Hansaja <ifire810@gmail.com>
2024-06-03 09:08:25 +0700

    code profiller

---
+++ codeProfiel.py
@@ -0,0 +1,19 @@
+import torch
+import torchvision
+
+def get_model():
+    return torchvision.models.resnet18(pretrained=True)
+
+def get_pred(model):
+    return model(torch.rand([1,3,224,224]))
+
+model = get_model()
+
+for i in range(1,10000):
+    get_pred(model)
+    print(i)
+    
+    
+
+    
+    
